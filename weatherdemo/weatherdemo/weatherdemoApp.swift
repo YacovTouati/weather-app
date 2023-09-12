28106fce8ba49288279619a7c246b2ce5c243b7d
Yacov Youati <136307501+yakovtouatiBALINK@users.noreply.github.com>
2023-09-12 13:08:03 +0300

    All app

---
+++ weatherdemo/weatherdemo/weatherdemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherdemoApp.swift
+//  weatherdemo
+//
+//  Created by yacov touati on 11/09/2023.
+//
+
+import SwiftUI
+
+@main
+struct weatherdemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
